ca56ee60dc6f68ef4a164f285016a3241d6e4d26
Leonel Rivas <leonelrivas.rl@gmail.com>
2022-07-28 12:05:41 -0300

    DTOs & information validation

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,20 +1,28 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { CreateCarDto, UpdateCarDto } from './dto';
+import { Car } from './interfaces/car.interface';
 
 @Injectable()
 export class CarsService {
-  private cars = [
+  private cars: Car[] = [
     {
-      id: 1,
+      id: uuid(),
       brand: 'BMW',
       model: 'X5',
     },
     {
-      id: 2,
+      id: uuid(),
       brand: 'Audi',
       model: 'A4',
     },
     {
-      id: 3,
+      id: uuid(),
       brand: 'Mercedes',
       model: 'C class',
     },
@@ -24,11 +32,51 @@ export class CarsService {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
 
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+
+    this.cars.push(car);
+
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.findOneById(id);
+
+    if (updateCarDto.id && updateCarDto.id !== id) {
+      throw new BadRequestException(
+        `Car id '${updateCarDto.id}' does not match id '${id}'`,
+      );
+    }
+
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDB = {
+          ...carDB,
+          ...updateCarDto,
+          id,
+        };
+        return carDB;
+      }
+      return car;
+    });
+    return carDB;
+  }
+
+  delete(id: string) {
+    const car = this.findOneById(id);
+
+    this.cars = this.cars.filter((car) => car.id !== id);
   }
 }
